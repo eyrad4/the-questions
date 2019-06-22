14c57f5500f39a3afe3fb04931bd2bf7b51d226f
eyrad4 <eyrad4@gmail.com>
2019-06-22 20:08:02 +0300

    Created basic template

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,6 +8,9 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
+import Bulma from 'bulma';
+Vue.use(Bulma);
+
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
